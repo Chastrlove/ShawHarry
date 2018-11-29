601a460f0be4f11275589bf342a6c7d73a4d13fc
马晨杰 <309272985@qq.com>
2018-11-29 17:32:17 +0800

    update code-gen

--- swagger-codegen/code-gen.sh
+++ swagger-codegen/code-gen.sh
@@ -3,10 +3,6 @@ do
    if [ -f ../swagger/$file ]
    name="$(echo ${file%.*} | awk '{print tolower(substr($0,0,1))substr($0,2,length($0))}' )"
    then
-    java -jar ./swagger-codegen-cli.jar generate -i ../swagger/$file -l  typescript-fetch -t ./templates/typescript-fetch  -o ../src/api/
-#    java -jar ./swagger-codegen-cli.jar generate -i ./doc/$file -l objc -o ./temp/objcTemp/$name
-#    java -jar ./swagger-codegen-cli.jar generate -i ./doc/$file -l java -o ./temp/android/$name
-#    java -jar ./swagger-codegen-cli.jar generate -i ./doc/$file -l JavaSpring-Lszd -o ./temp/JavaSpring/$name
-#    node jsonSchema.js $file ./doc/$file ./temp/schema
+    java -jar ./swagger-codegen-cli.jar generate -i ../swagger/$file -l typescript-fetch -t ./templates/typescript-fetch  -o ../src/api/
    fi
 done
