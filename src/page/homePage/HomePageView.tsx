6d238bb1bf5928ed25771b3b27cc2c89817a8f4d
马晨杰 <309272985@qq.com>
2018-11-29 17:27:39 +0800

    update code-gen

--- src/page/homePage/HomePageView.tsx
+++ src/page/homePage/HomePageView.tsx
@@ -2,7 +2,17 @@ import * as React from "react";
 import * as style from "./homeStyle.pcss";
 
 export class HomePageView extends React.Component<any> {
+  public doAction = async () => {
+    const a = [new Promise((resolve) => resolve(123)), 23, 4];
+    for await (const p of a) {
+      console.log(p);
+    }
+  };
   public render() {
-    return <div className={style.love}>123</div>;
+    return (
+      <div className={style.love} onClick={this.doAction}>
+        123
+      </div>
+    );
   }
 }
